f68e7b65710c5686507921969fb692a5ec9f57bd
Per-Frode Pedersen <pfp@tuck.as>
2010-06-18 21:13:51 +0200

    Created Home

---
+++ FotoWeb/Home/Home.cs
@@ -0,0 +1,22 @@
+﻿using FubuMVC.Core.View;
+
+namespace FotoWeb
+{
+    public class HomeViewModel
+    {
+    }
+
+    public class HomeInputModel
+    {
+        
+    }
+    public class  HomeView : FubuPage<HomeViewModel>{}
+
+    public class HomeController
+    {
+        public HomeViewModel Home(HomeInputModel inputModel)
+        {
+            return new HomeViewModel();
+        }
+    }
+}
