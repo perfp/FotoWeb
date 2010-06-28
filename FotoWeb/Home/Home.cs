4531946431d772b8af8e982bfb55c65eb597a9f6
Per-Frode Pedersen <pfp@tuck.as>
2010-06-28 20:13:59 +0200

    spark views

--- FotoWeb/Home/Home.cs
+++ FotoWeb/Home/Home.cs
@@ -4,8 +4,15 @@ namespace FotoWeb.Home
 {
     public class HomeViewModel
     {
-    }
+        private string _image;
 
+        public string ImageUploader
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+    
     public class HomeController
     {
         public HomeViewModel Home()
